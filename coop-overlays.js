4636570954c43a35f22baa7879a5dd4e63dc441c
ernandes-sobreira <156730719+ernandes-sobreira@users.noreply.github.com>
2026-09-15 17:52:20 -0400

    feat: conclui overlays UFCG e UNIOESTE

--- coop-overlays.js
+++ coop-overlays.js
@@ -5,7 +5,9 @@ async function loadData(){
   'dados/enriquecimento_cluster7_2026.json',
   'dados/enriquecimento_ufpe_ufs_2026.json',
   'dados/enriquecimento_uff_2026.json',
-  'dados/enriquecimento_ufam_2026.json'
+  'dados/enriquecimento_ufam_2026.json',
+  'dados/enriquecimento_unioeste_2026.json',
+  'dados/enriquecimento_ufcg_2026.json'
  ];
  const req=[
   fetch('dados/docentes_base_2026.json?ts='+ts,{cache:'no-store'}),
